91a4c31bdc564d7502aabad5d502cd7c33e0c90f
victor <vapyhtin@gmail.com>
2016-05-27 14:56:38 +0200

    atlas packer

--- Googlecardboard_memorygame_tiger/Assets/RandomCards.cs
+++ Googlecardboard_memorygame_tiger/Assets/RandomCards.cs
@@ -3,22 +3,40 @@ using UnityEngine;
 
 public class RandomCards : MonoBehaviour
 {
-    private List<int> uniqueNumbers;
-
-    public Material skinAtlasMaterial;
-    public Material backfaceMaterial;
+    public Texture2D[] faceTexturesPack;
 
     public Mesh[] cardMeshes;
 
     [HideInInspector]
     public MemoryCard[] cardsInstances;
 
+    private Rect[] atlasRects;
+    private Material skinAtlasMaterial;
+    private Material backfaceMaterial;
+
+    private int ATLAS_TEX_COUNT = 8;
+    private int ATLAS_TEX_SIZE = 256;
+
     void Awake()
     {
         cardsInstances = GetComponentsInChildren<MemoryCard>();
 
-        uniqueNumbers = new List<int>();
+        Texture2D atlasTexture = new Texture2D(2048, 2048, TextureFormat.RGB24, true, true);
+        atlasTexture.wrapMode = TextureWrapMode.Clamp;
 
+        for (int i = 0; i < transform.childCount / 2; i++)
+        {
+            int row = i / ATLAS_TEX_COUNT;
+            int column = i % ATLAS_TEX_COUNT;
+
+            Color32[] colors = faceTexturesPack[i].GetPixels32();
+            atlasTexture.SetPixels32(column * ATLAS_TEX_SIZE, (ATLAS_TEX_COUNT - row - 1) * ATLAS_TEX_SIZE, ATLAS_TEX_SIZE, ATLAS_TEX_SIZE, colors);
+        }
+
+        atlasTexture.Apply(true, true);
+
+        skinAtlasMaterial = new Material(Shader.Find("Unlit/Texture"));
+        skinAtlasMaterial.mainTexture = atlasTexture;
 
         GenerateRandomList();
     }
@@ -26,7 +44,9 @@ public class RandomCards : MonoBehaviour
     public void GenerateRandomList()
     {
         int numberOfPairs = transform.childCount / 2;
+
         List<int> cardList = new List<int>();
+        List<int> uniqueNumbers = new List<int>();
 
         //first card
         for (int i = 0; i < numberOfPairs; i++)
@@ -55,7 +75,7 @@ public class RandomCards : MonoBehaviour
         for (int i = 0; i < cardsInstances.Length; i++)
         {
             cardsInstances[i].cardnumber = cardList[i];
-            cardsInstances[i].SetCardMesh(cardMeshes[i]);
+            cardsInstances[i].SetCardMesh(cardMeshes[cardList[i]]);
             cardsInstances[i].SetCardFaceMaterial(skinAtlasMaterial);
         }
     }
